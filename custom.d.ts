b4ad9e08d17f4025b977d36a5b0769953795bfc6
mloncar <martin.loncar@hivetech.hr>
2025-03-03 15:26:54 +0100

    feat: add initial project structure

---
+++ custom.d.ts
@@ -0,0 +1,8 @@
+declare module '@tailwindcss/vite' {
+    const tailwindcss: any;
+    export default tailwindcss;
+}
+
+declare module '@react-router/dev/vite' {
+    export const reactRouter: any;
+}
